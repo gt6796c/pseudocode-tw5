f52f6ed12fb7d08d16afdd8aee30d5bfb70d3ad5
Nathan Jones <nbjones@gmail.com>
2016-09-06 10:36:21 -0400

    updated to use rocklib and to note dependencies

--- wrapper.js
+++ wrapper.js
@@ -12,125 +12,12 @@ module-type: widget
      var uniqueID = 1;
 
     var Widget = require("$:/core/modules/widgets/widget.js").widget;
+    var Rocklib = require("$:/plugins/gt6796c/rocklib/widget-tools.js").rocklib;
     if ($tw.browser && !window.pseudocode) {
+        window.rocklib = new Rocklib();
         window.pseudocode = require("$:/plugins/gt6796c/pseudocode-tw5/pseudocode.js");
     }
 
-    function getScriptBody(src,tag)
-    {
-        var scriptBody= src.getAttribute("text", src.parseTreeNode.text || "");
-
-        if (src.parseTreeNode.text) {
-            scriptBody = src.parseTreeNode.text;
-        }
-        else if (src.parseTreeNode.children)
-        {
-            var kids = src.parseTreeNode.children;
-            for (var k in kids)
-            {
-                var kk = kids[k];
-                switch(kk.type)
-                {
-                    // no wikiparsing found
-                    case "text":
-                        scriptBody += kk.text; break;
-                    // internal link, make it a permalink reference
-                    case "link":
-                        scriptBody += "#" + kk.children[0].text; break;
-                    // -- gets interpreted as an HTML element, we don't want that
-                    case "entity":
-                        switch(kk.entity)
-                        {
-                            case "&ndash;":
-                                scriptBody+="--"; break;
-                        };
-                        break;
-                    // just re-write it back out
-                    case "element":
-                        switch (kk.tag)
-                        {
-                            case "a":
-                                scriptBody+=kk.children[0].text;
-                        }
-                };
-            }
-        }
-        else {
-            scriptBody = "";
-        }
-
-        return scriptBody;
-
-    };
-
-    function getCanvas(src,tag)
-    {
-        var height = src.getAttribute("height");
-        var width = src.getAttribute("width");
-
-        var divNode = src.document.createElement("div");
-        var style = "";
-        if (height) style +=" height:"+height;
-        if (width) style +=" width:"+width;
-        if (style) divNode.setAttribute("style", style);
-        divNode.setAttribute("id", tag+"_" + uniqueID);
-
-        uniqueID++;
-        return divNode;
-    };
-
-    function getOptions(src, tag, options)
-    {
-        try {
-            // try to set options from fields on tiddler first
-            // [tag-xxx]
-            var tt = src.getVariable('currentTiddler');
-            if (tt) {
-                var t = src.wiki.getTiddler(tt);
-                if (t)
-                {
-                    for (var f in t.fields) {
-                        var fi = f.indexOf(tag);
-                        if (fi != 0) continue;
-                        var k = f.substring(tag.length+1);
-                        var v = t.fields[f];
-                        // try as JSON
-                        try {
-                            options[k] = JSON.parse(v);
-                        }
-                            // last attempt is just a string
-                        catch (ex) {
-                            options[k] = v;
-                        }
-                    }
-                }
-            }
-            // treat any attributes as JSON representations of options
-            // for the object
-            for (var att in src.attributes) {
-                // we already used these in the div definition
-                if (att == "height" || att == "width") continue;
-
-                var attval = src.getAttribute(att);
-                // allow for data from named tiddlers
-                if ($tw.wiki.tiddlerExists(attval))
-                {
-                    var data = $tw.wiki.getTiddlerData(attval);
-                    options[att] = data;
-                }
-                else {
-                    // try as JSON
-                    try { options[att] = JSON.parse(attval); }
-                    // last attempt is just a string
-                    catch (ex) { options[att] = attval; }
-                }
-            }
-        }
-        catch (ex) { console.error(ex); }
-
-        return options;
-
-    };
 
     var PseudocodeWidget = function(parseTreeNode, options) {
         this.initialise(parseTreeNode, options);
@@ -147,13 +34,18 @@ module-type: widget
         this.execute();
 
         var tag = 'pseudocode';
-        var scriptBody = getScriptBody(this,tag);
-        var divNode = getCanvas(this,tag);
+        var scriptBody = rocklib.getScriptBody(this,"text");
+        var divNode = rocklib.getCanvas(this,tag);
         try {
             var options = {}; // lineNumber: true};
-            getOptions(this, tag, options);
-            console.error(pseudocode.renderToString(scriptBody,options));
-            pseudocode.render(scriptBody, divNode, options);
+            rocklib.getOptions(this, tag, options);
+            var prolog = "\\begin{algorithm}\n"
+            console.error(options['caption'])
+            if (options['caption'])
+              prolog += "\\caption{" + options['caption'] + "}\n"
+            prolog += "\\begin{algorithmic}"
+            var postlog ="\\end{algorithmic}\n\\end{algorithm}"
+            pseudocode.render(prolog + scriptBody + postlog, divNode, options);
         }
         catch(ex)
         {
